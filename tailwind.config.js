ced7473776a0e214a6b02a63c2168231a040add4
iMac <imac@iMacs-iMac.local>
2023-07-12 23:13:26 +0530

    Final Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens:{
+        'ssm': '537px',
+        'sssm':'380px'
+      }
     },
   },
   plugins: [],
